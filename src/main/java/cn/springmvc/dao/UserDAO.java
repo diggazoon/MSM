730e09166e9a286d60a47c568ac17a582541806c
digga-dong <zhanguo.dong@163.com>
2014-10-31 09:49:41 +0800

    commit

---
+++ src/main/java/cn/springmvc/dao/UserDAO.java
@@ -0,0 +1,20 @@
+package cn.springmvc.dao;
+
+import cn.springmvc.model.User;
+
+
+public interface UserDAO {
+
+	/**
+	 * 娣诲姞鏂扮敤鎴�
+	 * @param user
+	 * @return
+	 */
+	public int insertUser(User user);
+	
+	
+	//@Select("SELECT * FROM blog WHERE id = #{id}") 
+	//Blog selectBlog(int id);
+	
+	
+}
